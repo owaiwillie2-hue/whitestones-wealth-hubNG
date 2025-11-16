fd3a18fa0ecb0f71f9fa2737ef39751fe0fcef5b
Developer <developer@whitestones.com>
2025-11-16 16:02:04 +0100

    fix: remove duplicate provider wrapping in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import { LanguageProvider } from "./contexts/LanguageContext";
 
-createRoot(document.getElementById("root")!).render(
-  <ThemeProvider>
-    <LanguageProvider>
-      <App />
-    </LanguageProvider>
-  </ThemeProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
